db48f9b47bf2c3c152e3967cb6e9c6922f4a6090
Vladimir Malygin <vldmr.one@gmail.com>
2020-02-06 22:06:54 +0300

    add create article route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule), pathMatch: 'full' },
   { path: 'article/:id', loadChildren: () => import('./article-page/article-page.module').then(m => m.ArticlePageModule) },
   { path: 'edit', loadChildren: () => import('./edit-page/edit-page.module').then(m => m.EditPageModule) },
+  { path: 'create', loadChildren: () => import('./edit-page/edit-page.module').then(m => m.EditPageModule) },
   { path: '404', loadChildren: () => import('./not-found/not-found.component').then(m => m.NotFoundComponent) },
   { path: '**', redirectTo: '404' },
 ];
